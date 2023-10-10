39e83826b9298546f9f05d7053a3bdfdfd8f79a0
El1te-Discord <60895481+El1te-Discord@users.noreply.github.com>
2023-10-10 19:30:10 +0100

    Changelog and logo management.

---
+++ src/main/java/com/txch/narcotics/event/ModEvents.java
@@ -0,0 +1,33 @@
+package com.txch.narcotics.event;
+
+import com.txch.narcotics.Narcotics;
+import com.txch.narcotics.item.ModItems;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraftforge.event.village.VillagerTradesEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
+
+@Mod.EventBusSubscriber(modid = Narcotics.MODID)
+public class ModEvents {
+    @SubscribeEvent
+    public static void addCustomTrades(VillagerTradesEvent event) {
+        if(event.getType() == VillagerProfession.FARMER) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+
+            // Level 2 Trades
+            trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 2),
+                    new ItemStack(ModItems.COCA_SEEDS.get(), 16),
+                    10, 8, 0.2f
+            ));
+
+        }
+    }
+}
